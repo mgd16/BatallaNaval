1be7a6f7c53ce504cf4d2f1f157bea4e86971c3d
Javier <nottu1990@gmail.com>
2013-11-05 10:58:45 -0600

    Initial Commit

---
+++ BatallaNaval/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BatallaNaval
+//
+//  Created by Javier on 11/5/13.
+//  Copyright (c) 2013 Javier. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
